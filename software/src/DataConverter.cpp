2fd6644c04c27863d65b9c3e9e6f61e0825a7e58
dchmist <twistudc@gmail.com>
2020-09-29 22:19:40 +0200

    Added if statement and switch insert_iterator to inserter function (result is the same but code is more clearly now)

--- software/src/DataConverter.cpp
+++ software/src/DataConverter.cpp
@@ -4,9 +4,11 @@
 
 std::vector<uint8_t> DataConverter::toBytes(const std::string &str)
 {
+    if(str.size()==0)
+        return {};
     std::vector<uint8_t> result;
     auto castToByte = [](const char x){return static_cast<uint8_t>(x);};
-    std::transform(str.begin(), str.end(), std::insert_iterator< std::vector<uint8_t> >(result, result.begin()), castToByte);
+    std::transform(str.begin(), str.end(), std::inserter(result, result.begin()), castToByte);
     return result;
 }
 std::vector<uint8_t> DataConverter::toBytes(const char *data, size_t size)
@@ -18,8 +20,10 @@ std::vector<uint8_t> DataConverter::toBytes(const char *data, size_t size)
 }
 std::string DataConverter::toString(const std::vector<uint8_t> &buffer)
 {
+    if(buffer.size())
+        return std::string("");
     std::string str;
     auto castToChar = [](const uint8_t x){return static_cast<char>(x);};
-    std::transform(buffer.begin(), buffer.end(), std::insert_iterator< std::string >(str, str.begin()), castToChar);
+    std::transform(buffer.begin(), buffer.end(), std::inserter(str, str.begin()), castToChar);
     return str;
 }
